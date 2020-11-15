83e5039bafa7ea46abfafa5b7b563b9cfdded394
shoffkarim <k4r1mch1k@gmail.com>
2020-11-15 16:20:49 +0500

    create product and category components

---
+++ src/shop/Shop.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { CategoryNavigation } from "./CategoryNavigation";
+import { ProductList } from "./ProductList";
+
+export class Shop extends Component {
+
+  render() {
+    return <div className="container-fluid">
+      <div className="row">
+        <div className="col bg-dark text-white">
+          <div className="navbar-brand">
+            SPORTS STORE
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3 p-2">
+          <CategoryNavigation baseUrl="/shop/products" categories={ this.props.categories }></CategoryNavigation>
+        </div>
+        <div className="col-9 p-2">
+          <ProductList products={ this.props.products }></ProductList>
+        </div>
+      </div>
+    </div>
+  }
+}
